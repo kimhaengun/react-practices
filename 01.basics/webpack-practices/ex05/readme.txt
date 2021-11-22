88317c4df858a50212ae54ee26c50ff7f5540acf
kimhaengun <chxl89@naver.com>
2021-11-22 13:17:58 +0900

    webpack #3

---
+++ 01.basics/webpack-practices/ex05/readme.txt
@@ -0,0 +1,4 @@
+npm i -D webpack webpack-cli webpack-dev-server css-loader style-loader sass-loader node-sass
+
+### sass부분
+sass-loader node-sass
---
+++ 01.basics/webpack-practices/ex05/src/App.js
@@ -0,0 +1,11 @@
+import './assets/scss/App.scss';
+
+const App = function(){
+    const app = document.createElement('h1');
+    //css 사용방법
+    app.className = 'Header';
+    app.textContent = 'Welcome to React';
+    return app; //DOM app를 만들어서 리턴해줌
+}
+
+export {App};
