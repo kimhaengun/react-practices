359154ce88aa21ec4e6f57f1a72ab6794a4c34f4
kimhaengun <chxl89@naver.com>
2021-11-26 14:08:04 +0900

    칸반보드 #1

---
+++ 02.component/kanbanboard/src/01/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Kanbanboard from './KanbanBoard';
+export default function(){
+    return (
+        <div id = 'App'>
+            <Kanbanboard/>
+        </div>
+    )
+}
