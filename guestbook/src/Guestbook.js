0c2bd9d0d9bee49b4385ede76ac0b05db3539cc0
kimhaengun <chxl89@naver.com>
2021-11-29 15:37:50 +0900

    Guestbook component

--- guestbook/src/Guestbook.js
+++ guestbook/src/Guestbook.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import GuestbookForm from './GuestbookForm';
+import GuestbookView from './GuestbookView';
+import './assets/css/styles.css';
 
 export default function() {
-    return (
-        <div id='App'>
-          <h1>Guestbook</h1>
-        </div>
+    return (  
+      <div className='Guestbook'>
+          <h1>방명록</h1>
+          <GuestbookForm />
+          <GuestbookView/>
+      </div>
     )
 }
