b531cae898f5d6fdcdd90e4853879ec786eeed90
kimhaengun <chxl89@naver.com>
2021-12-02 13:13:15 +0900

    emaillist 데이터 찾기(Search)

--- emaillist/src/App.js
+++ emaillist/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './assets/scss/App.scss';
 import Emaillist from './Emaillist';
 import RegisterForm from './RegisterForm';
@@ -7,11 +7,18 @@ import Searchbar from './Searchbar';
 import data from './assets/json/data.json';
 
 export default function() {
+
+    const [keyword,setKeyword] = useState('');
+    const notifyKeywordChanged = (keyword) => {
+        console.log(keyword);
+        setKeyword(keyword);
+    }
+
     return (
-        <div className={'App'}>
+        <div className={'App'}>0
             <RegisterForm/>
-            <Searchbar/>
-            <Emaillist emails={data}/>
+            <Searchbar callback={notifyKeywordChanged}/>
+            <Emaillist keyword={keyword} emails={data}/>
         </div>
     )
 }
--- emaillist/src/Searchbar.js
+++ emaillist/src/Searchbar.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './assets/scss/Searchbar.scss';
-const Searchbar = () => {
+const Searchbar = ({callback}) => {
+
+    //제어방식 (value, onchange())
+    const [keyword,setKeyword] = useState('');
+    
+    const onInputChanged = (e) =>{
+        setKeyword(e.target.value);
+        //부모한테 값 전달 callback
+        callback(e.target.value)
+    };
+    
     return (
         <div className={styles.Searchbar}>
-            찾기: <input type='text' placeholder='찾기'/>
+            찾기: <input type='text' placeholder='찾기' value={keyword} onChange={onInputChanged}/>
         </div>
     );
 };
