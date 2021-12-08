a8a4d104d6ddbbd667da0ed98d1ad79c9556b1b9
kimhaengun <chxl89@naver.com>
2021-12-08 10:58:23 +0900

    route

---
+++ 03.routing/src/01/component/Guestbook.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Guestbook() {
+    return (
+        <Layout>
+            <div>
+                <h1>Guestbook</h1>
+            </div>
+        </Layout>
+    );
+}
---
+++ 03.routing/src/01/component/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Main() {
+    return (
+        <Layout>
+            <div>
+                <h1>Main</h1>
+            </div>
+        </Layout>
+    );
+}
